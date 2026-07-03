d7cc2cd72ecde112b706741e7abe74340551d456
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-07-03 22:06:40 +0500

    Math EXpression

--- script.js
+++ script.js
@@ -10,3 +10,18 @@
 
 //  Question 2 
 
+  document.write( "<h1>" + "Question 2"  + "<br>" + "</h1>");
+
+  var sub = num1 - num2;
+  var multiply= num1 * num2;
+  var divide = num1 / num2;
+  var mode = num1 % num2;
+
+  document.write("Subtraction of " + num1  + " " +"and " + num2 + " " + "is "
+     + sub + "<br>");
+  document.write("Multiplication of " + num1  + " " +"and " + num2 + " " + 
+    "is " + multiply + "<br>");
+  document.write(" Division of " + num1  + " " +"and " + num2 + " " + 
+  "is " +divide + "<br>");
+  document.write(" Modulus of " + num1  + " " +"and " + num2 + " " + 
+    "is " + mode);
