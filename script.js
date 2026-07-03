7ae8857c9f5be4fca373e3d40eb706cae338fee5
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-07-03 22:34:11 +0500

    Math Expression

--- script.js
+++ script.js
@@ -1,44 +1,72 @@
 // Question 1
- 
- document.write( "<h1>" + "Question 1"  + "<br>" + "</h1>");
 
- var num1 = 3;
- var num2 = 5;
- var add = num1 + num2;
+document.write("<h1>" + "Question 1" + "<br>" + "</h1>");
 
- document.write("Sum of " + num1  + " " +"and " + num2 + " " + "is " + add);
+var num1 = 3;
+var num2 = 5;
+var add = num1 + num2;
+
+document.write("Sum of " + num1 + " " + "and " + num2 + " " + "is " + add);
 
 //  Question 2 
 
-  document.write( "<h1>" + "Question 2"  + "<br>" + "</h1>");
+document.write("<h1>" + "Question 2" + "<br>" + "</h1>");
 
-  var sub = num1 - num2;
-  var multiply= num1 * num2;
-  var divide = num1 / num2;
-  var mode = num1 % num2;
+var sub = num1 - num2;
+var multiply = num1 * num2;
+var divide = num1 / num2;
+var mode = num1 % num2;
 
-  document.write("Subtraction of " + num1  + " " +"and " + num2 + " " + "is "
-     + sub + "<br>");
-  document.write("Multiplication of " + num1  + " " +"and " + num2 + " " + 
+document.write("Subtraction of " + num1 + " " + "and " + num2 + " " + "is "
+    + sub + "<br>");
+document.write("Multiplication of " + num1 + " " + "and " + num2 + " " +
     "is " + multiply + "<br>");
-  document.write(" Division of " + num1  + " " +"and " + num2 + " " + 
-  "is " +divide + "<br>");
-  document.write(" Modulus of " + num1  + " " +"and " + num2 + " " + 
+document.write(" Division of " + num1 + " " + "and " + num2 + " " +
+    "is " + divide + "<br>");
+document.write(" Modulus of " + num1 + " " + "and " + num2 + " " +
     "is " + mode);
 
 // Question 3 
 
- document.write( "<h1>" + "Question 3"  + "<br>" + "</h1>");
+document.write("<h1>" + "Question 3" + "<br>" + "</h1>");
 
- var increment
- document.write("Value after variable declaration is " + increment + "<br>");
- var intialize = 5;
- document.write("Initialize value:" + intialize +"<br>");
- var valueAfter = ++intialize;
- document.write("Value after increment is " + valueAfter  + "<br>");
- var addVar = valueAfter + 7;
- document.write("Value after addition is " + addVar + "<br>"); 
- var decrement = --addVar;
- document.write("Decrement Value:" + decrement + "<br>"); 
- var divideValue = decrement % 3;
- document.write("The reminder is:" + divideValue); 
+var increment
+document.write("Value after variable declaration is " + increment + "<br>");
+var intialize = 5;
+document.write("Initialize value:" + intialize + "<br>");
+var valueAfter = ++intialize;
+document.write("Value after increment is " + valueAfter + "<br>");
+var addVar = valueAfter + 7;
+document.write("Value after addition is " + addVar + "<br>");
+var decrement = --addVar;
+document.write("Decrement Value:" + decrement + "<br>");
+var divideValue = decrement % 3;
+document.write("The reminder is:" + divideValue);
+
+
+//  Question 4 
+
+
+document.write("<h1>" + "Question 4" + "<br>" + "</h1>");
+var ticket = 600;
+var ticketCost = 600 * 5;
+document.write("The Cost of buying 5 tickets is " + ticketCost + "PKR");
+
+//  Question 5 
+
+document.write("<h1>" + "Question 5" + "<br>" + "</h1>");
+
+var table = "4";
+
+document.write("Table of 4" + "<br>");
+document.write(table + " x " + 1 + " = " + table*1 + "<br>");
+document.write(table + " x " + 2 + " = " + table*2 + "<br>");
+document.write(table + " x " + 3 + " = " + table*3 + "<br>");
+document.write(table + " x " + 4 + " = " + table*4 + "<br>");
+document.write(table + " x " + 5 + " = " + table*5 + "<br>");
+document.write(table + " x " + 6 + " = " + table*6 + "<br>");
+document.write(table + " x " + 7 + " = " + table*7 + "<br>");
+document.write(table + " x " + 8 + " = " + table*8 + "<br>");
+document.write(table + " x " + 9 + " = " + table*9 + "<br>");
+document.write(table + " x " + 10 + " = " + table*10 + "<br>");
+
